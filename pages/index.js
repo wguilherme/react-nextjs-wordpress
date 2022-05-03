c3c04071eb74ff55106a69eb1a9e476139164776
W. Guilherme <wguilhermecriativo@gmail.com>
2022-05-03 16:11:05 -0300

    update

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Navigation from '../components/Navigation'
+import { Fragment } from 'react'
+
+export default () => (
+   <Fragment>
+      <Navigation />
+      <h1>Your new server-side rendered React.js app!</h1>
+   </Fragment>
+)
